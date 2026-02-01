9640c3bf647f709c038dae8ce25e2b04b9a8452c
rlsn <yumo1996@gmail.com>
2026-02-01 22:06:36 +0900

    stop tracking dist folder

--- dist/cli.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Coneko CLI - Main entry point
- */
-export {};
-//# sourceMappingURL=cli.d.ts.map
--- dist/types/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * Core type definitions for coneko-cli
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=index.js.map
